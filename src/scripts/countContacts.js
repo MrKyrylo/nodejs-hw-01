0ab55583a286b1598719dc8afb9645f618e34a9b
MrKyrylo <kirilka294729@gmail.com>
2024-09-06 18:48:46 +0100

    1

---
+++ src/scripts/countContacts.js
@@ -0,0 +1,13 @@
+import { PATH_DB } from '../constants/contacts.js';
+
+import fs from 'node:fs/promises';
+
+export const countContacts = async () => {
+  let getFile;
+  getFile = await fs.readFile(PATH_DB, 'utf8');
+  const contactsArray = JSON.parse(getFile);
+  const contactsAmount = contactsArray.length;
+  return contactsAmount;
+};
+
+console.log(await countContacts());
---
+++ src/scripts/getAllContacts.js
@@ -0,0 +1,12 @@
+import { PATH_DB } from '../constants/contacts.js';
+
+import fs from 'node:fs/promises';
+
+export const getAllContacts = async () => {
+  let getFile;
+  getFile = await fs.readFile(PATH_DB, 'utf8');
+  const contactsArray = JSON.parse(getFile);
+  return contactsArray;
+};
+
+console.log(await getAllContacts());
